6d6f37186df4a907253fff3d49cb3aeae586ada1
sriintecai <srikanth@intecai.in>
2024-10-07 19:58:16 +0530

    Made changes

--- src/Pages/LandingPage/HomeComponents/TopProductsComp/Scroll_Cards_TP.tsx
+++ src/Pages/LandingPage/HomeComponents/TopProductsComp/Scroll_Cards_TP.tsx
@@ -37,10 +37,10 @@ export default function Scroll_Cards_TP() {
       <div className="relative pl-5">
         <div ref={feedbackRef} className="flex overflow-x-scroll lg:no-scrollbar gap-6 flex-row w-full overflow-hidden">
           {spclche?.map((item: SpclChe, index: number) => (
-            <div key={index} className="min-w-fit border-1 rounded-md">
+            <div key={index} className="min-w-fit border-1 rounded-xl bg-white">
               <Image className="w-[35vh] h-[35vh]" src={item?.img} />
-              <h1 className="font-bold text-md lg:text-xl">{item?.title}</h1>
-              <h1 className="text-lg">{item?.price}</h1>
+              <h1 className="ml-4 mt-1 font-bold text-md lg:text-xl">{item?.title}</h1>
+              <h1 className="ml-4 mt-1 text-lg">{item?.price}</h1>
             </div>
           ))}
         </div>
